c7a7473a87c11298fb21fdf745796995615890dc
Tuhin Mondal <126494222+tuhinmon@users.noreply.github.com>
2024-07-23 09:29:33 +0530

    Time: 14 ms (81.76%), Space: 26.9 MB (44.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,18 +3,37 @@ public:
     int majorityElement(vector<int>& nums) {
         // sort(nums.begin(),nums.end());
         // return nums[nums.size()/2+1];
+        
+        // T.C =0(N);
+        // S.C =0(N);
+//         int ans = 0;
+//         unordered_map<int,int>mpp;
+//         for(int i=0;i<nums.size();i++){
+//             mpp[nums[i]]++;
+//         }
+//         for(auto x:mpp){
+//             if(x.second>nums.size()/2){
+//                 ans = x.first;
+//                 break;
+//             }
+//         }
+//         return ans;
         int ans = 0;
-        unordered_map<int,int>mpp;
+        int ele =0;
         for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(auto x:mpp){
-            if(x.second>nums.size()/2){
-                ans = x.first;
-                break;
+            if(ans==0){
+                ele = nums[i];
+                ans++;
+            } 
+            else if(ele==nums[i]){
+                ans++;
+                
+            }
+            else {
+                ans--;
             }
         }
-        return ans;
+        return ele;
         
         
     }
