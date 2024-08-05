69122dc79b23f1da1bd71ff63cc57445649d2a94
Tuhin Mondal <126494222+tuhinmon@users.noreply.github.com>
2024-08-05 09:38:42 +0530

    Time: 1 ms (42.47%), Space: 10.1 MB (14.92%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -27,66 +27,58 @@ public:
 //            inorder(root,ans);
 //            return ans;
            
-           
          // itterative way 
         // T.C =0(N);
         // S.C =0(N);
-         vector<int>ans;
-         stack<TreeNode*>st;
-         TreeNode*node = root;
-         while(!st.empty()|| node!=NULL){
-             if(node!=NULL){
-                 st.push(node);
-                 node= node->left;
-             }
-             else{
-                 node = st.top();
-                 st.pop();
-                 ans.push_back(node->val);
-                 node = node->right;
+//          vector<int>ans;
+//          stack<TreeNode*>st;
+//          TreeNode*node = root;
+//          while(!st.empty()|| node!=NULL){
+//              if(node!=NULL){
+//                  st.push(node);
+//                  node= node->left;
+//              }
+//              else{
+//                  node = st.top();
+//                  st.pop();
+//                  ans.push_back(node->val);
+//                  node = node->right;
                  
-             }
-         }
-           return ans;
+//              }
+//          }
+//            return ans;
 
 
 
         // morris inorder tarversal 
         // T.C =0(N);
         // S.C =0(1);
-//         vector<int>ans;
-//         TreeNode*cur = root;
-//         while(cur!=NULL){
-//             if(cur->left==NULL){
-//                 ans.push_back(cur->val);
-//                 cur=cur->right;
-
-//             }
-//             else{
-//                 TreeNode*prev = cur->left;
-//                 while( prev->right!=NULL && prev->right!=cur){
-//                     prev = prev->right;
-//                 }
-//                 if(prev->right==NULL){
-//                     prev->right = cur;
-//                     cur = cur->left;
-
-
-//                 }
-//                 else{
-//                     prev->right = NULL;
-//                     ans.push_back(cur->val);
-//                     cur = cur->right;
-//                 }
-
-
-//             }
-
-
-//         }
-//         return ans;
-
-
+           TreeNode*cur = root;
+           vector<int>ans;
+           while(cur!=NULL){
+               if(cur->left==NULL){
+                   ans.push_back(cur->val);
+                   cur = cur->right;
+              }
+               else{
+                   TreeNode*temp = cur->left;
+                   while(temp->right!=NULL && temp->right!=cur){
+                       temp = temp->right;
+                   }
+                   if(temp->right==NULL){
+                       temp->right = cur;
+                       cur = cur->left;
+                   }
+                   else{
+                       temp->right = NULL;
+                       ans.push_back(cur->val);
+                       cur = cur->right;
+                   }
+               }
+           }
+           return ans;
+           
+           
         
     }
 };
