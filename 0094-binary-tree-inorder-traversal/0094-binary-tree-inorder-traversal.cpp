c59ce7ad1a2139e5b705ecea14ce0065ec49e8fd
Tuhin Mondal <126494222+tuhinmon@users.noreply.github.com>
2024-08-05 09:08:07 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (14.92%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,45 +11,43 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode*root,vector<int>&ans){
-        if(root==NULL){
-            return;
-        }
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-    }
+    // void inorder(TreeNode*root,vector<int>&ans){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     inorder(root->left,ans);
+    //     ans.push_back(root->val);
+    //     inorder(root->right,ans);
+    // }
        vector<int> inorderTraversal(TreeNode* root) {
-           vector<int>ans;
-           inorder(root,ans);
-           return ans;
-            
-
-
-
-
-
-
-        // itterative way 
+           // T.C = 0(N);
+           // S.C = 0(N);
+           // recursion method
+//            vector<int>ans;
+//            inorder(root,ans);
+//            return ans;
+           
+           
+         // itterative way 
         // T.C =0(N);
         // S.C =0(N);
-
-        // vector<int>ans;
-        // stack<TreeNode*>st;
-        // TreeNode*node = root;
-        // while(!st.empty() || node!=NULL){
-        //     if(node!=NULL){
-        //         st.push(node);
-        //         node=node->left;
-        //     }
-        //     else{
-        //         node = st.top();
-        //         st.pop();
-        //         ans.push_back(node->val);
-        //         node = node->right;
-        //     }
-        // }
-        // return ans;
+         vector<int>ans;
+         stack<TreeNode*>st;
+         TreeNode*node = root;
+         while(!st.empty()|| node!=NULL){
+             if(node!=NULL){
+                 st.push(node);
+                 node= node->left;
+             }
+             else{
+                 node = st.top();
+                 st.pop();
+                 ans.push_back(node->val);
+                 node = node->right;
+                 
+             }
+         }
+           return ans;
 
 
 
