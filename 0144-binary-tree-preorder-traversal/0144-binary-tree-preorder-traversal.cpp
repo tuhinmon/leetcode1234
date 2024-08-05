27b2114f3781a6b235d7cb0bfa35609aeb96b9ff
Tuhin Mondal <126494222+tuhinmon@users.noreply.github.com>
2024-08-05 10:06:16 +0530

    Time: 4 ms (31.89%), Space: 10 MB (43.58%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -28,21 +28,54 @@ public:
         // return ans;
         
         // itterative approach 
+        
+        // T.C =0(N);
+        // S.C =0(N);
+        // vector<int>ans;
+        // stack<TreeNode*>st;
+        // if(root==NULL){
+        //     return ans;
+        // }
+        // st.push(root);
+        // while(!st.empty()){
+        //     TreeNode*temp = st.top();
+        //     st.pop();
+        //     ans.push_back(temp->val);
+        //     if(temp->right!=NULL){
+        //         st.push(temp->right);
+        //     }
+        //     if(temp->left!=NULL){
+        //         st.push(temp->left);
+        //     }
+        // }
+        // return ans;
+        
+        
+        
+        // morrics preorder tarversal 
+        // T.C =0(N);
+        // S.C =0(1);
         vector<int>ans;
-        stack<TreeNode*>st;
-        if(root==NULL){
-            return ans;
-        }
-        st.push(root);
-        while(!st.empty()){
-            TreeNode*temp = st.top();
-            st.pop();
-            ans.push_back(temp->val);
-            if(temp->right!=NULL){
-                st.push(temp->right);
+        TreeNode*cur = root;
+        while(cur!=NULL){
+            if(cur->left==NULL){
+                ans.push_back(cur->val);
+                cur = cur->right;
             }
-            if(temp->left!=NULL){
-                st.push(temp->left);
+            else{
+                TreeNode*temp = cur->left;
+                while(temp->right!=NULL && temp->right!=cur){
+                    temp = temp->right;
+                }
+                if(temp->right==NULL){
+                    temp->right = cur;
+                    ans.push_back(cur->val);
+                    cur = cur->left;
+                }
+                else{
+                    temp->right = NULL;
+                    cur = cur->right;
+                }
             }
         }
         return ans;
