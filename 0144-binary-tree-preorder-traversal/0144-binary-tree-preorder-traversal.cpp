934ed036412ad55b40a9eedd59f95ad844a065ef
Tuhin Mondal <126494222+tuhinmon@users.noreply.github.com>
2024-08-05 09:51:52 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (17.35%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,19 +11,42 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode*root,vector<int>&ans){
-        if(root==NULL){
-            return;
-        }
-        ans.push_back(root->val);
-        preorder(root->left,ans);
-        preorder(root->right,ans);
-    }
+    // void preorder(TreeNode*root,vector<int>&ans){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     ans.push_back(root->val);
+    //     preorder(root->left,ans);
+    //     preorder(root->right,ans);
+    // }
     vector<int> preorderTraversal(TreeNode* root) {
         // recursion 
+        // T.C =0(N);
+        // S.C =0(N);
+        // vector<int>ans;
+        // preorder(root,ans);
+        // return ans;
+        
+        // itterative approach 
         vector<int>ans;
-        preorder(root,ans);
+        stack<TreeNode*>st;
+        if(root==NULL){
+            return ans;
+        }
+        st.push(root);
+        while(!st.empty()){
+            TreeNode*temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if(temp->right!=NULL){
+                st.push(temp->right);
+            }
+            if(temp->left!=NULL){
+                st.push(temp->left);
+            }
+        }
         return ans;
+        
         
     }
 };
